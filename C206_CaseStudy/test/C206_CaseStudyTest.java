c1861777043b750c0a75c6ca5553581102d4b35e
19021825 <19021825@DESKTOP-OME1OV2.lan>
2020-08-20 13:54:20 +0800

    Default Case Test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -8,43 +8,18 @@ import org.junit.Test;
 
 public class C206_CaseStudyTest {
 	
-	private Procedure cc1;
-	private Procedure cc2;
-	private ArrayList<Procedure> procedureList;
+
 	@Before
 	public void setUp() throws Exception {
-		cc1 = new Procedure(1, "refund",
-				"Step 1: Verify product returns request, Step 2: Verify customer's credentials and order information, Step 3: Process refund by reqward points");
-		cc2 = new Procedure(2, "exchange",
-				"Step 1: Verify product exchange request, Step 2: Verify customer's credentials and order information, Step 3: exchange product");
-		//ArrayList<Procedure> procedureList = new ArrayList<Procedure>();
-		procedureList = new ArrayList<Procedure>();
+
 	}
 	
-	public void addProcedure() {
-		
-		// Item list is not null, so that can add a new item - boundary
-		assertNotNull("Check if there is valid Camcorder arraylist to add to", procedureList);
-		//Object procedureList;
-		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
-		//The item just added is as same as the first item of the list
-		ProcedureExecution.inputprocedure(procedureList, cc1);
-		assertEquals("Check that Camcorder arraylist size is 1", 1, procedureList.size());
-		assertSame("Check that Camcorder is added", cc1, procedureList.get(0));
-		
-		//Add another item. test The size of the list is 2? -normal
-		//The item just added is as same as the second item of the list
-		ProcedureExecution.inputprocedure(procedureList, cc2);
-		assertEquals("Check that Camcorder arraylist size is 2", 2, procedureList.size());
-		assertSame("Check that Camcorder is added", cc2, procedureList.get(1));
-	}
+
 	
 
 	@After
 	public void tearDown() throws Exception {
-		cc1 = null;
-		cc2 = null;
-		procedureList = null;
+
 	}
 
 	@Test
