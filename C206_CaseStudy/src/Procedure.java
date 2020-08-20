bab2e2c4d2ac58df965467df6b1fb85d7ee552f8
19023645 <19023645@LAPTOP-5NLIMOIN>
2020-08-20 13:47:08 +0800

    procedure code(shunkai)

---
+++ C206_CaseStudy/src/Procedure.java
@@ -0,0 +1,32 @@
+
+
+
+public class Procedure {
+
+int Procedure_id;
+//test
+
+String Procedure_name;
+String Procedure_description;
+
+public Procedure(int procedure_id, String procedure_name, String procedure_description) {
+	this.Procedure_id = procedure_id;
+	this.Procedure_name = procedure_name;
+	this.Procedure_description = procedure_description;
+}
+
+public int getProcedure_id() {
+	return Procedure_id;
+}
+public String getProcedure_name() {
+	return Procedure_name;
+}
+public String getProcedure_description() {
+	return Procedure_description;
+}
+
+
+
+
+
+}
