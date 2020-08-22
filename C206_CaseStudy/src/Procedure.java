f623974b884b8750298cb503da0d1fc8730bc2e7
19023645 <19023645@LAPTOP-5NLIMOIN>
2020-08-22 11:49:58 +0800

    procedure2

--- C206_CaseStudy/src/Procedure.java
+++ C206_CaseStudy/src/Procedure.java
@@ -1,6 +1,5 @@
 
 
-
 public class Procedure {
 
 int Procedure_id;
@@ -8,13 +7,16 @@ int Procedure_id;
 
 String Procedure_name;
 String Procedure_description;
-
-public Procedure(int procedure_id, String procedure_name, String procedure_description) {
+String comment;
+public Procedure(int procedure_id, String procedure_name, String procedure_description, String comment) {
+	
 	this.Procedure_id = procedure_id;
 	this.Procedure_name = procedure_name;
 	this.Procedure_description = procedure_description;
+	this.comment = comment;
 }
 
+//test
 public int getProcedure_id() {
 	return Procedure_id;
 }
@@ -23,6 +25,12 @@ public String getProcedure_name() {
 }
 public String getProcedure_description() {
 	return Procedure_description;
+}
+public String getComment() {
+	return comment;
+}
+public void setComment(String comment) {
+	this.comment = comment;
 }
 
 
--- C206_CaseStudy/src/ProcedureExecution.java
+++ C206_CaseStudy/src/ProcedureExecution.java
@@ -1,18 +1,18 @@
 
 
-
 import java.util.ArrayList;
 
 public class ProcedureExecution {
-
+//terst
 	public static void main(String[] args) {
 		//try
 		ArrayList<Procedure> procedureList = new ArrayList<Procedure>();
 
 		procedureList.add(new Procedure(1, "refund",
-				"Step 1: Verify product returns request, Step 2: Verify customer's credentials and order information, Step 3: Process refund by reqward points"));
+				"Step 1: Verify product returns request, Step 2: Verify customer's credentials and order information, Step 3: Process refund by reward points", "Completed"));
 		procedureList.add(new Procedure(2, "exchange",
-				"Step 1: Verify product exchange request, Step 2: Verify customer's credentials and order information, Step 3: exchange product"));
+				"Step 1: Verify product exchange request, Step 2: Verify customer's credentials and order information, Step 3: exchange product", "Processing"));
+	//	remarkList.add(new Procedure("Good job"));
 
 		int option = 0;
 
@@ -31,7 +31,7 @@ public class ProcedureExecution {
 				
 			} else if (option == 3) {
 				// Delete item
-				deleteprocedure(procedureList, getprocedure());
+				deleteprocedure(procedureList);
 
 			} else {
 				System.out.println("Invalid option");
@@ -53,10 +53,10 @@ public class ProcedureExecution {
 
 	public static void viewAllprocedure(ArrayList<Procedure> procedureList) {
 		String output = "";
-		output += String.format("%-10s %-30s %s\n", "Procedure ID", "Procedure Name", "Procedure Description");
+		output += String.format("%-20s %-20s %-150s %-100s\n", "Procedure ID", "Procedure Name", "Procedure Description", "Remarks");
 		
 		for (Procedure i : procedureList) {
-			output += String.format("%-10d %-40s %s20\n", i.getProcedure_id(), i.getProcedure_name(), i.getProcedure_description());
+			output += String.format("%-20d %-20s %-150s %-100s\n", i.getProcedure_id(), i.getProcedure_name(), i.getProcedure_description(), i.getComment());
 		}
 		System.out.println(output);
 	}
@@ -65,34 +65,42 @@ public class ProcedureExecution {
 		int id = Helper.readInt("Enter id > ");
 		String name = Helper.readString("Enter name > ");
 		String description = Helper.readString("Enter description > ");
+		String comment = Helper.readString("Enter your remark > ");
 
-		procedureList.add(new Procedure(id, name, description));
+		procedureList.add(new Procedure(id, name, description, comment));
 		System.out.println("Procedure Added");
 	}
 
-	public static Procedure getprocedure() {
+
+//test
+	public static void deleteprocedure(ArrayList<Procedure> procedureList) {
 		int id = Helper.readInt("Enter id > ");
-		String name = Helper.readString("Enter name > ");
-		String description = Helper.readString("Enter description > ");
-
-		Procedure Pro = new Procedure(id, name, description);
-		return Pro;
-	}
-
-	public static void deleteprocedure(ArrayList<Procedure> procedureList, Procedure Pro) {
+		boolean isDelete = false;
 		for (int i = 0; i < procedureList.size(); i++) {
-			if (procedureList.contains(Pro)) {
-				procedureList.remove(Pro);
-			} else {
-				System.out.println("Procedure does not exist");
+			if (procedureList.get(i).getProcedure_id() == id) {
+				isDelete = true;
+				procedureList.remove(i);
+				System.out.println("Procedure deleted");
 			}
 		}
-		System.out.println("Procedure deleted");
+		if(isDelete == false) {
+			System.out.println("Delete Failed!!");
+		}
+		
 	}
 
 	public static void inputprocedure(Object procedureList, Procedure cc1) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	public static void updateremark(ArrayList<Procedure> procedureList)  {
+		int chooseid = Helper.readInt("Enter the id that need to be updated");
+		String updatecomment = Helper.readString("Enter the updated remark");
+		for (int i = 0; i < procedureList.size(); i++) {
+			if (procedureList.get(i).getProcedure_id() == chooseid) {
+				procedureList.get(i).setComment(updatecomment);
+			}
+		}
+	}
 }
